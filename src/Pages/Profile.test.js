1e2ef5d41bdda9fd8e7a9b83e9f44f72e85769db
Bwin44 <bqnguy21@cougarnet.uh.edu>
2024-07-19 21:12:12 -0500

    Profile.test.js and volunteerhistory.test.js Was Not Found
    These two files were not found when I pulled so I added them again

---
+++ src/Pages/Profile.test.js
@@ -0,0 +1,56 @@
+zimport React from "react";
+import Navigation from '../Components/Navigation';
+import "../styles2.css";
+
+
+
+//create prop function to use values from ProfileManage
+const Profile = (props) => {
+    const { fullName, getAdd, getCity, getState, getZip, skillArray, getPref, dateArray} = props;
+    
+    return(
+        <div>
+            <Navigation />
+            <div className="profileContainer5">
+
+                <div className="profileContainer3">
+
+                    <div className="profileContainer1">
+                        <h1 className="header">{fullName}'s Profile</h1>
+                        <div className="location">
+                        <p>Resides in: {getAdd}, {getCity}, {getState}, {getZip}</p>
+                        </div>
+                    </div>
+
+                    <div className="profileContainer2">
+                      <div className="image">Profile image</div>
+                    </div>
+
+                </div>
+              
+                <div className="profileContainer4">
+
+                    <div className="personalPrefDiv">
+                      <h3>Skills:</h3>
+                      <p> {skillArray} </p>
+                      <h3>Preferences: </h3>
+                      <p>{getPref}</p>
+                      <h3>Dates available:</h3>
+                      <p> {dateArray} </p>
+                    </div>
+                    <br/>
+                    <button className="pmbutton">
+                      <a href="/profile/profilemanagement">Profile Management</a>
+                    </button>
+
+                </div>
+
+            </div>
+            
+            
+            
+        </div>
+    )
+}
+
+export default Profile;
