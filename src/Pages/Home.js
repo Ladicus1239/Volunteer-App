48d641bb7045d872681aea27d6b5bebda111e95c
AnthonyR712 <anthonyram712@gmail.com>
2024-08-06 13:53:27 -0500

    Fixed home page & volunteer history page
    Made the heading in volunteer history match the other pages . Also remove "homepage" from the home page and changed the slogan.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Navigation from '../Components/Navigation';
+import React from "react";
+import Navigation from "../Components/Navigation";
 import "../styles.css";
 import logo from "../Media/volunteer.png";
 import { useAuth } from "../context/AuthContext";
@@ -9,11 +9,11 @@ export default function Home() {
   return (
     <div>
       <Navigation />
-      <h1 className="pageTitle">Home Page</h1><br/>
-      <img src={logo} alt="Logo" className='logo'/>
+      <img src={logo} alt="Logo" className="logo" />
       <p className="center-text">
-        Welcome {currentUser && currentUser.email}<br/>
-        Save people and maybe animals
+        Welcome {currentUser && currentUser.email}
+        <br />
+        Save people, animals, and communities.
       </p>
     </div>
   );
